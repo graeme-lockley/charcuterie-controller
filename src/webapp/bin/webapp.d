9600ac0fcfc251a94468b1d90ef55bd4f5ec9f15
Graeme Lockley <graemel@no9.co.za>
2014-04-25 14:19:15 +0200

    Changed the redirects to ensure that the logging is dropped into /var/log

--- src/webapp/bin/webapp.d
+++ src/webapp/bin/webapp.d
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 cd /home/pi/webapp/app
-ruby lib/webapp.rb
+
+sudo sh -c 'echo > /var/log/webapp.log'
+sudo chown pi /var/log/webapp.log
+
+ruby lib/webapp.rb > /var/log/webapp.log 2>&1
 
