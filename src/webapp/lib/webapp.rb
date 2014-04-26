551ed72ad3b2f2581bb38e683c3f7abf31dd5953
Graeme Lockley <graemel@no9.co.za>
2014-04-26 23:16:12 +0200

    Simplified the /api/collections result to not include a status element

--- src/webapp/lib/webapp.rb
+++ src/webapp/lib/webapp.rb
@@ -8,18 +8,15 @@ set :public_folder, File.dirname(__FILE__) + '/../data'
 set :bind, '0.0.0.0'
 
 get '/' do
-	File.read(File.join('data', 'index.html'))
+  File.read(File.join('data', 'index.html'))
 end
 
 get '/api/containers' do
-	content_type :json
-	{
-		:status => 'success',
-		:containers => CCModel::Thingy.instance.containers.map { |x|
-			{
-				:id => x.id,
-				:name => x.name
-			}
-		}
-	}.to_json
+  content_type :json
+  CCModel::Thingy.instance.containers.map { |x|
+    {
+        :id => x.id,
+        :name => x.name
+    }
+  }.to_json
 end
