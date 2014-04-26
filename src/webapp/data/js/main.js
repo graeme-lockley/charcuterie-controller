f7344a30aa4c73ac31fafa451c106c1554367b50
Graeme Lockley <graemel@no9.co.za>
2014-04-26 23:16:37 +0200

    Removed logging code

--- src/webapp/data/js/main.js
+++ src/webapp/data/js/main.js
@@ -7,17 +7,11 @@ require.config({
     }
 });
 
-console.log("main: before the require");
-
 require([
     'app',
     'views/containers/list'
 ], function (App, ContainerListView) {
-    console.log("main: before app initialize");
-
     App.initialize();
-
-    console.log("main: we have arrived");
 
     var containerListView = new ContainerListView();
     containerListView.render();
