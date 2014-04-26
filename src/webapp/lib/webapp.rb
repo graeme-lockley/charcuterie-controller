5c88220d0c997b5eccfbc2059e5350bee080b572
graeme.lockley <Password1>
2014-04-26 16:01:10 +0200

    Create the rest service /api/containers

--- src/webapp/lib/webapp.rb
+++ src/webapp/lib/webapp.rb
@@ -1,9 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
+
+require_relative 'ccmodel'
 
 set :public_folder, File.dirname(__FILE__) + '/../data'
 set :bind, '0.0.0.0'
 
 get '/' do
 	File.read(File.join('data', 'index.html'))
+end
+
+get '/api/containers' do
+	content_type :json
+	{
+		:status => 'success',
+		:containers => CCModel::Thingy.instance.containers.map { |x|
+			{
+				:id => x.id,
+				:name => x.name
+			}
+		}
+	}.to_json
 end
