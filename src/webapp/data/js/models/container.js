1ec0bb6117952e232c5a6245aba8212236204677
Graeme Lockley <graemel@no9.co.za>
2014-04-27 12:26:33 +0200

    Added a method to return all of the elements associated with this container.

--- src/webapp/data/js/models/container.js
+++ src/webapp/data/js/models/container.js
@@ -1,10 +1,18 @@
 define([
     'underscore',
-    'backbone'
-], function (_, Backbone) {
+    'backbone',
+    'collections/elements'
+], function (_, Backbone, ElementCollection) {
     return Backbone.Model.extend({
         defaults: {
             name: "Default Name"
+        },
+
+        elements: function () {
+            var elements = new ElementCollection();
+            elements.url = "/api/containers/" + this.id + "/elements";
+            elements.fetch({async: false});
+            return elements;
         }
     });
 });
