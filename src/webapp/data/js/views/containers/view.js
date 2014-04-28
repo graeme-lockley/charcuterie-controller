3f8af0d7a3dec2f176048fe1c84f8497cabae04b
Graeme Lockley <graemel@no9.co.za>
2014-04-28 21:25:15 +0200

    Added the ability to return to the apps opening page - note that this had to be resolved through managing a cyclic dependency.

--- src/webapp/data/js/views/containers/view.js
+++ src/webapp/data/js/views/containers/view.js
@@ -2,12 +2,23 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'views/containers/list',
     'text!/templates/container/view.html'
-], function ($, _, Backbone, ContainersViewTemplate) {
+], function ($, _, Backbone, ContainerListView, ContainersViewTemplate) {
     return Backbone.View.extend({
         el: "#content",
         render: function () {
             this.$el.html(_.template(ContainersViewTemplate, { model: this.model }));
+        },
+        events: {
+            "click #back": "clickBack"
+        },
+        clickBack: function () {
+            if (!ContainerListView) {
+                ContainerListView = require("views/containers/list");
+            }
+
+            new ContainerListView().render();
         }
     });
 });
