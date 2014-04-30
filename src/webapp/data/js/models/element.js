c52539edaf239414c863ab1dc709b08159f16fd9
Graeme Lockley <graemel@no9.co.za>
2014-04-30 07:47:54 +0200

    Added the ability for an element to return its contained elements.

--- src/webapp/data/js/models/element.js
+++ src/webapp/data/js/models/element.js
@@ -1,10 +1,23 @@
 define([
     'underscore',
-    'backbone'
-], function (_, Backbone) {
+    'backbone',
+    'collections/elements'
+], function (_, Backbone, ElementCollection) {
     return Backbone.Model.extend({
         defaults: {
             name: "Default Name"
+        },
+        newElementCollection: function () {
+            if (!ElementCollection) {
+                ElementCollection = require("collections/elements");
+            }
+            return new ElementCollection();
+        },
+        elements: function () {
+            var elements = this.newElementCollection();
+            elements.url = "/api/containers/" + this.id + "/elements";
+            elements.fetch({async: false});
+            return elements;
         }
     });
 });
