83b7bb292416d23288e8e626a6c7154b5fc03337
Graeme Lockley <graemel@no9.co.za>
2014-04-30 07:48:24 +0200

    Exploded the view out into a view class for each of the element types.

--- src/webapp/data/js/views/containers/view.js
+++ src/webapp/data/js/views/containers/view.js
@@ -3,13 +3,32 @@ define([
     'underscore',
     'backbone',
     'views/containers/list',
-    'text!/templates/container/view.html'
+    'text!/templates/container/view.html',
+    'views/containers/view_Chamber',
+    'views/containers/view_TemperatureProbe',
+    'views/containers/view_HumidityProbe'
 ], function ($, _, Backbone, ContainerListView, ContainersViewTemplate) {
     return Backbone.View.extend({
         el: "#content",
+        newElementView: function (element) {
+            var elementType = element.get("class").substr(9);
+            console.log("views/containers/view_" + elementType);
+            var elementView = require("views/containers/view_" + elementType);
+
+            return new elementView({model: element});
+
+        },
         render: function () {
-            this.$el.html(_.template(ContainersViewTemplate, { model: this.model }));
-            this.$el.find('ul').listview().listview('refresh');
+            var view = this;
+
+            view.$el.html(_.template(ContainersViewTemplate, { model: view.model }));
+
+            view.model.elements().each(function (element) {
+                var elementView = view.newElementView(element);
+                view.$el.find("ul").append(elementView.render().el);
+            });
+
+            view.$el.find('ul').listview().listview('refresh');
         },
         events: {
             "click #back": "clickBack"
---
+++ src/webapp/data/js/views/containers/view_TemperatureProbe.js
@@ -0,0 +1,20 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function ($, _, Backbone) {
+    return Backbone.View.extend({
+        tagName: 'li',
+        template: _.template('<a id="#link"><%= item.get("name") %><span class="ui-li-count" style="color: <%= item.get("ragStatus")%>">Temperature Probe</span></a>'),
+        render: function () {
+            $(this.el).html(this.template({ item: this.model}));
+            return this;
+        },
+        events: {
+            "click": "click"
+        },
+        click: function () {
+            alert("click: " + this.model.get("name"));
+        }
+    });
+});
